aae90ee886ab2caeae1e305a795a2fa8a1f12cbb
mae <hiroki.mae@rakus.co.jp>
2017-08-23 16:54:20 +0900

    xmlのjarをwarに変えました

--- src/main/java/jp/co/rakus/ec_cite/App.java
+++ src/main/java/jp/co/rakus/ec_cite/App.java
@@ -2,17 +2,21 @@ package jp.co.rakus.ec_cite;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 /**
  * SpringApplication起動
  *
  */
 @SpringBootApplication
-public class App 
-{
-    public static void main( String[] args )
-    {
-    		SpringApplication.run(App.class, args);
-    		
-    }
+public class App extends SpringBootServletInitializer{
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
+		return application.sources(App.class);
+	}
 }
